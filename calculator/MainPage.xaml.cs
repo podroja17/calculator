9f62e7b6937df225d1d81647a2df9a57807628f6
Jan Podroužek <podroja17@sps-prosek.cz>
2020-10-11 17:57:47 +0200

    Initial commit (not so much initial actually)

---
+++ calculator/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Xamarin.Forms;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Data;
+
+namespace calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        List<String> operations = new List<string>() { "÷", "×", "-", "+" };
+
+        public MainPage()
+        {
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                this.Padding = new Thickness(0, 20, 0, 0);
+            }
+
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            String buttonText = ((Button)sender).Text;
+
+            if (buttonText == "=")
+            {
+                Calculate();
+                return;
+            }
+
+            if (buttonText == "C")
+            {
+                Erase();
+                return;
+            }
+
+            UpdateDisplay(buttonText);
+        }
+
+        void UpdateDisplay(string character)
+        {
+            if (display.Text == "0" && character != ".")
+            {
+                display.Text = character;
+            } else {
+                display.Text += character;
+            }
+            
+        }
+
+        void Calculate()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                display.Text = dt.Compute(display.Text.Replace('÷', '*'), "").ToString();
+            } catch (Exception e)
+            {
+                DisplayAlert("OOF", "Velký špatný" , "Odbouchnout");
+            }
+            
+        }
+
+        void Erase()
+        {
+            display.Text = "0";
+        }
+    }
+}
